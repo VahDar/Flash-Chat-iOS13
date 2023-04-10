cae6ff16a09accab18d3448304cdb6ba10fc21bc
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-04-10 21:25:00 +0300

    my flash chat

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,25 +1,120 @@
-//
-//  ChatViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        navigationItem.hidesBackButton = true
+        title = Constans.appName
+        tableView.register(UINib(nibName: Constans.cellNibName, bundle: nil), forCellReuseIdentifier: Constans.cellIdentifier)
+        
+        loadMessages()
     }
     
+    func loadMessages() {
+        
+        
+        db.collection(Constans.FStore.collectionName)
+            .order(by: Constans.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+            
+            self.messages = []
+            
+            if let error = error {
+                print("There was an issue retrieving data from Firestore. \(error)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                      let data = doc.data()
+                        if let messagesender = data[Constans.FStore.senderField] as? String, let messageBody = data[Constans.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messagesender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text,
+           let messageSender = Auth.auth().currentUser?.email {
+            db.collection(Constans.FStore.collectionName).addDocument(data: [Constans.FStore.senderField: messageSender, Constans.FStore.bodyField: messageBody, Constans.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to firesore, \(e)")
+                } else {
+                    
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
+                    
+                }
+            }
+        }
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        
+        do {
+          try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+        
+    }
+    
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constans.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = message.body
+        
+        //This is a message from the current user
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImagView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: Constans.BrandColors.lighBlue)
+            cell.label.textColor = UIColor(named: Constans.BrandColors.blue)
+        }
+        //This is the message from another sender
+        else {
+            cell.leftImagView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: Constans.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: Constans.BrandColors.purple)
+        }
+        
+        return cell
+    }
+}
+
+
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,22 +1,40 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        animatedTitel()
        
     }
-    
+        
+    func animatedTitel() {
+        
+        titleLabel.text = Constans.appName
+        
+//        titleLabel.text = ""
+//        var characterIndex = 0.0
+//        let titelText = "⚡️FlashChat"
+//        for letter in titelText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            characterIndex += 1
+//        }
+    }
 
 }
